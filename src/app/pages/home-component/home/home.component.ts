6c95673b73ea9395382395a76a4489ca697a7923
Roides Javier Cruz Lara <Roides.J.Cruz.-ND@disney.com>
2020-02-12 12:15:59 -0600

    add landing elements animation

--- src/app/pages/home-component/home/home.component.ts
+++ src/app/pages/home-component/home/home.component.ts
@@ -3,31 +3,33 @@ import { Component, OnInit, ElementRef, Renderer2 } from '@angular/core';
 import { HomeConstants } from './home.constants';
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+    selector: 'app-home',
+    templateUrl: './home.component.html',
+    styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-  constants = HomeConstants;
-  figuresState = [];
+    constants = HomeConstants;
+    figuresState = [];
 
-  constructor() { }
+    constructor() { }
 
-  ngOnInit() {
-    this.constants.MODELS_IMGS.forEach(() => {
-      this.figuresState.push(false);
-    });
-  }
+    ngOnInit() {
+        this.constants.MODELS_IMGS.forEach(() => {
+            this.figuresState.push(false);
+        });
+    }
+    translateY(element: ElementRef, renderer?: Renderer2, props?: { factor: number }): void {
+        const scrollY = window.pageYOffset;
+        const { factor } = props;
 
-  translateY(element: ElementRef, renderer?: Renderer2, props?: { factor: number }): void {
-    const scrollY = window.pageYOffset;
-    const { factor } = props;
+        renderer.setStyle(element.nativeElement, 'transform', `translate(0px, ${scrollY / factor}%)`);
+    }
 
-    renderer.setStyle(element.nativeElement, 'transform', `translate(0px, ${scrollY / factor}%)`);
-  }
-
-  setFigureState(state: boolean, i: number,): void {
-    this.figuresState[i] = state;
-  }
-
+    setFigureState(state: boolean, i: number,): void {
+        if (state) {
+            setTimeout(() => {
+                this.figuresState[i] = state;
+            }, 150 * (i + 1));
+        }
+    }
 }
