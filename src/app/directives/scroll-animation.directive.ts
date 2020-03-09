e72f79769ed016b6ae810480de41114bd4ee7627
Roides Javier Cruz Lara <roidescruzlara@gmail.com>
2020-03-09 11:18:54 -0600

    Revert "[PT-1] - Parallax Header Isolation"

--- src/app/directives/scroll-animation.directive.ts
+++ src/app/directives/scroll-animation.directive.ts
@@ -1,10 +1,10 @@
 import { Directive, ElementRef, Renderer2, HostListener, Input, Output, EventEmitter } from '@angular/core';
 
 @Directive({
-    selector: '[appParallax]'
+    selector: '[appScrollAnimation]'
 })
-export class ParallaxDirective {
-    @Input('appParallax') animation: Function;
+export class ScrollAnimationDirective {
+    @Input('appScrollAnimation') animation: Function;
     @Input('scrolledChecker') scrolledChecker: Function;
     @Input('animationExtras') extras: string;
     @Input('checkIfScrolled') checkIfScrolled: boolean;
